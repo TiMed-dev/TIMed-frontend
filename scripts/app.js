f8f3fc9b380b74c3294ec9479022e175b987112a
andfoy <andfoy@gmail.com>
2016-11-09 10:57:32 -0500

    Minor changes in file structure

--- scripts/app.js
+++ scripts/app.js
@@ -22,69 +22,69 @@
   .state('base', {
     abstract: true,
     url: '',
-    templateUrl: 'views/base.html'
+    templateUrl: 'static/views/base.html'
   })
   .state('login', {
     url: '/login',
     parent: 'base',
-    templateUrl: 'views/login.html',
+    templateUrl: 'static/views/login.html',
     controller: 'LoginCtrl'
   })
   .state('dashboard', {
     url: '/dashboard',
     parent: 'base',
-    templateUrl: 'views/dashboard.html',
+    templateUrl: 'static/views/dashboard.html',
     controller: 'DashboardCtrl'
   })
   .state('inicio', {
     url: '/inicio',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/inicio.html'
+    templateUrl: 'static/views/dashboard/inicio.html'
   })
   .state('informacion', {
     url: '/informacion',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/informacion.html'
+    templateUrl: 'static/views/dashboard/informacion.html'
   })
   .state('resultados', {
     url: '/resultados',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/resultados.html'
+    templateUrl: 'static/views/dashboard/resultados.html'
   })
   .state('files', {
     url: '/files',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/files.html'
+    templateUrl: 'static/views/dashboard/files.html'
   })
   .state('informacion1', {
     url: '/informacion/1',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/informacion1.html'
+    templateUrl: 'static/views/dashboard/informacion1.html'
   })
   .state('informacion2', {
     url: '/informacion/2',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/informacion2.html'
+    templateUrl: 'static/views/dashboard/informacion2.html'
   })
   .state('informacion3', {
     url: '/informacion/3',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/informacion3.html'
+    templateUrl: 'static/views/dashboard/informacion3.html'
   })
   .state('resultados1', {
     url: '/resultados/1',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/resultados1.html'
+    templateUrl: 'static/views/dashboard/resultados1.html'
   })
   .state('resultados2', {
     url: '/resultados/2',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/resultados2.html'
+    templateUrl: 'static/views/dashboard/resultados2.html'
   })
   .state('resultados3', {
     url: '/resultados/3',
     parent: 'dashboard',
-    templateUrl: 'views/dashboard/resultados3.html'
+    templateUrl: 'static/views/dashboard/resultados3.html'
   });
 
 });
